2ba36d17a6c43be0a53a0a1c12861b1bd751d742
kenyanroot <ndedaanaco@gmail.com>
2023-08-21 12:12:55 -0700

    fix

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,33 +1,32 @@
-#include <stdio.h>
-
+#include "main.h"
 /**
- * _atoi - convert a string to an integer
- * @s: string to convert
- *
- * Return: the integer value of the string, or 0 if no numbers are prese
+ * _atoi - converts a string to an integer.
+ * @s: the string to convert
+ * Return: the converted string.
  */
 int _atoi(char *s)
 {
-    int result = 0;
-    int sign = 1;
-    int started = 0; /* This is to keep track if we've started reading numbers */
-    int i; // Declare loop counter variable here
+	short boolean;
+	int i, minus, result;
 
-    for (i = 0; s[i] != '\0'; i++)
-    {
-        /* Check for a sign */
-        if (s[i] == '-' && !started)
-            sign *= -1;
-        else if (s[i] == '+' && !started)
-            continue;
-        else if (s[i] >= '0' && s[i] <= '9')
-        {
-            started = 1;
-            result = result * 10 + (s[i] - '0');
-        }
-        else if (started) /* Break if we encounter a non-number after numbers */
-            break;
-    }
+	i = minus = result = boolean = 0;
+	minus = -1;
 
-    return (result * sign);
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+			minus *= -1;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			result *= 10;
+			result -= (s[i] - '0');
+			boolean = 1;
+		}
+		else if (boolean == 1)
+			break;
+		i++;
+	}
+	result *= minus;
+	return (result);
 }
