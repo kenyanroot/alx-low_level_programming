c17059d0b4c1baa33b83ede0b74b34a49caa972d
kenyanroot <ndedaanaco@gmail.com>
2023-08-21 12:27:04 -0700

    possible fix for 11

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,56 +1,49 @@
 #include <stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include <time.h>
-
-#define PASSWORD_LENGTH 8
-
 /**
- * generate_random_password - Generate a random password
- * @length: length of the password to generate
- *
- * Return: A pointer to the generated password
+ * checksum - executes checksum
+ * @s: input char
+ * Return: checksum
  */
-char *generate_random_password(int length)
+unsigned long checksum(char *s)
 {
-    /* Characters to use in the password */
-    char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+";
-    char *password = malloc(length + 1);
-    int i;  /* Declare loop counter variable here */
-    
-    if (!password)
-    {
-        return NULL;
-    }
-
-    for (i = 0; i < length; i++)
-    {
-        int random_index = rand() % (sizeof(charset) - 1);
-        password[i] = charset[random_index];
-    }
-
-    password[length] = '\0';
-
-    return password;
+unsigned long sum = 0;
+while (*s != 0)
+{
+	sum += *s;
+	s++;
 }
-
+return (sum);
+}
+/**
+ * main - prints password for crakme
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-    char *password;  /* Declare variable at the beginning of the function */
+	char alpha[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQSTUVWXYZ";
+	char s[33];
+	unsigned long sum;
+	int i, flag = 0;
 
-    /* Seed random number generator */
-    srand(time(NULL));
-
-    password = generate_random_password(PASSWORD_LENGTH);
-
-    if (password)
-    {
-        printf("Generated Password: %s\n", password);
-        free(password);
-    }
-    else
-    {
-        printf("Failed to generate a password.\n");
-    }
-
-    return 0;
+	srand(time(NULL));
+	while (flag == 0)
+	{
+		for (i = 0; i < 33; i++)
+		{
+			s[i] = alpha[rand() % (sizeof(alpha) - 1)];
+		}
+		s[i] = '\0';
+		sum = checksum(s);
+		if (sum == 2772)
+		{
+			flag = 1;
+			printf("%s", s);
+		}
+	}
+return (0);
 }
+
