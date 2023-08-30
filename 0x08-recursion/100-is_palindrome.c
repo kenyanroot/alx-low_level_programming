6dfdf8959eb4f191e2221450d810df2910d154ab
kenyanroot <ndedaanaco@gmail.com>
2023-08-30 12:16:44 -0700

    fix

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -9,11 +9,11 @@
  */
 int check_pal(char *s, int i, int len)
 {
-    if (*(s + i) != *(s + len - 1))
-        return (0);
-    if (i >= len)
-        return (1);
-    return (check_pal(s, i + 1, len - 1));
+	if (*(s + i) != *(s + len - 1))
+		return (0);
+	if (i >= len)
+		return (1);
+	return (check_pal(s, i + 1, len - 1));
 }
 
 /**
@@ -23,9 +23,9 @@ int check_pal(char *s, int i, int len)
  */
 int _strlen_recursion(char *s)
 {
-    if (*s == '\0')
-        return (0);
-    return (1 + _strlen_recursion(s + 1));
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
 }
 
 /**
@@ -35,7 +35,7 @@ int _strlen_recursion(char *s)
  */
 int is_palindrome(char *s)
 {
-    if (*s == 0)
-        return (1);
-    return (check_pal(s, 0, _strlen_recursion(s)));
+	if (*s == 0)
+		return (1);
+	return (check_pal(s, 0, _strlen_recursion(s)));
 }
