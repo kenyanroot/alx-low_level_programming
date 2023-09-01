34686ce9238e94e2b24a4169d0084415af0161d2
kenyanroot <ndedaanaco@gmail.com>
2023-09-01 15:30:11 -0700

    pheewgit add .

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints minimal number of coin change
+ * @argc: number of command line arguement
+ * @argv: array of command line arguementt
+ * Return: (0)success 1(failure)
+ */
+int main(int argc, char *argv[])
+{
+	int cents, coins;
+
+	if (argc == 1 || argc > 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	cents = atoi(argv[1]);
+
+	while (cents > 0)
+	{
+		if (cents >= 25)
+			cents -= 25;
+		else if (cents >= 10)
+			cents -= 10;
+		else if (cents >= 5)
+			cents -= 5;
+		else if (cents >= 2)
+			cents -= 2;
+		else if (cents >= 1)
+			cents -= 1;
+		coins += 1;
+
+	}
+	printf("%d\n", coins);
+	return (0);
+}
