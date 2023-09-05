b8a9fbf68d899c0b423fc4a31ba6979b30c31950
kenyanroot <ndedaanaco@gmail.com>
2023-09-05 15:00:26 -0700

    jj

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -37,6 +37,24 @@ void free_everything(char **string_array, int count)
 		free(string_array[--count]);
 	free(string_array);
 }
+/**
+ * alloc_word - Allocate memory for a word and fill it in
+ * @start: The start of the word in the original string
+ * @len: The length of the word
+ *
+ * Return: A pointer to the newly allocated string, or NULL on failure
+ */
+char *alloc_word(char *start, int len)
+{
+	int i;
+	char *word = malloc(len + 1);
+	if (word == NULL)
+		return (NULL);
+	for (i = 0; i < len; i++)
+		word[i] = start[i];
+	word[i] = '\0';
+	return (word);
+}
 
 /**
  * strtow - Split a string into words
@@ -46,8 +64,7 @@ void free_everything(char **string_array, int count)
  */
 char **strtow(char *str)
 {
-	int total_words = number(str);
-	int word_index = 0, char_index = 0, length = 0;
+	int total_words = number(str), word_index = 0, length = 0;
 	char **words, *current_word;
 
 	if (str == NULL || *str == 0 || total_words == 0)
@@ -69,18 +86,12 @@ char **strtow(char *str)
 				length++;
 				str++;
 			}
-			words[word_index] = malloc((length + 1) * sizeof(char));
+			words[word_index] = alloc_word(current_word, length);
 			if (words[word_index] == NULL)
 			{
 				free_everything(words, word_index);
 				return (NULL);
 			}
-			for (char_index = 0; char_index < length; char_index++)
-			{
-				words[word_index][char_index] = *current_word;
-				current_word++;
-			}
-			words[word_index][char_index] = '\0';
 			word_index++;
 			length = 0;
 		}
