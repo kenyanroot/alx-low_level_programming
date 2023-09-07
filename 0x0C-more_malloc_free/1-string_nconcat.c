e3eb21ad62a3441dc58572a0ed4de78c0eec7010
kenyanroot <ndedaanaco@gmail.com>
2023-09-07 08:19:47 -0700

    kk

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,68 +1,44 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 /**
- * string_nconcat - Concatenates two strings up to n characters.
- * @s1: The first string.
- * @s2: The second string.
- * @n: The number of characters to take from s2.
+ * string_nconcat - Concatenate two strings using n amount of s2
+ * @s1: First string
+ * @s2: String to add to end of s1
+ * @n: Amount of s2 to add to s1
  *
- * Return: A pointer to the newly allocated space in memory, or NULL on failure.
+ * Return: pointer to new area in memory, NULL if it fails
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    char *new_str;
-    unsigned int len1 = 0, len2 = 0, i, j;
+	char *nstr, *empt;
+	unsigned int i, len, j;
+	unsigned int size;
 
-    /* Handle NULL strings as empty strings */
-    if (s1 == NULL)
-        s1 = "";
-    if (s2 == NULL)
-        s2 = "";
-
-    /* Find the length of the strings */
-    while (s1[len1])
-        len1++;
-    while (s2[len2])
-        len2++;
-
-    /* If n is greater or equal to the length of s2, use the entire s2 */
-    if (n >= len2)
-        n = len2;
-
-    /* Allocate memory for the new string */
-    new_str = malloc(len1 + n + 1);
-    if (new_str == NULL)
-        return (NULL);
-
-    /* Copy s1 into the new string */
-    for (i = 0; i < len1; i++)
-        new_str[i] = s1[i];
-
-    /* Copy up to n characters from s2 into the new string */
-    for (j = 0; j < n; j++)
-        new_str[i + j] = s2[j];
-
-    /* Null-terminate the new string */
-    new_str[i + j] = '\0';
-
-    return (new_str);
-}
-
-/* Test the function */
-int main(void)
-{
-    char *s1 = "Hello, ";
-    char *s2 = "World!";
-    char *concat;
-
-    concat = string_nconcat(s1, s2, 3);
-    if (concat != NULL)
-    {
-        printf("Concatenated string: %s\n", concat);
-        free(concat);
-    }
-
-    return (0);
+	len = 0;
+	empt = "";
+	if (s1 == NULL)
+		s1 = empt;
+	if (s2 == NULL)
+		s2 = empt;
+	while (s1[len] != '\0')
+		len++;
+	size = (len + n) * sizeof(*nstr);
+	nstr = malloc(size + 1);
+	if (nstr == NULL)
+		return (NULL);
+	i = 0;
+	while (i < size && s1[i] != '\0')
+	{
+		nstr[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (i < size && s2[j] != '\0')
+	{
+		nstr[i] = s2[j];
+		i++;
+		j++;
+	}
+	nstr[i] = '\0';
+	return (nstr);
 }
