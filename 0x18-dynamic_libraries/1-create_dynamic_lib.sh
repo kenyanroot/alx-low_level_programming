0f2ec693d8e01e2dca96964fa52d90aec503bc35
kenyanroot <ndedaanaco@gmail.com>
2023-11-15 12:12:27 -0800

    updates

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-# Script to create a dynamic library from all .c files in the current directory
-
-# Compile all .c files in the current directory into object files
-gcc -Wall -Werror -Wextra -pedantic -c *.c
-
-# Create a dynamic library 'liball.so' from all the object files
+gcc -fPIC -c *.c
 gcc -shared -o liball.so *.o
-
-# Clean up by removing the object files
-rm -f *.o
